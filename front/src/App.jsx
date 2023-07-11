00a849b351e585a331a5c43ff6b20817d67d8bf2
alexandrebdry <alexbaudry0@gmail.com>
2023-07-11 17:42:15 +0200

    Feature Init Front
    adding wrapper to scroll to top

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,24 @@
 import AuthProvider from "./providers/AuthProvider.jsx";
 import Router from "./router/Router.jsx";
+import {useLocation} from "react-router-dom";
+import {useLayoutEffect} from "react";
+
+const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+}
 function App() {
 
 
   return (
-    <AuthProvider>
-        <Router></Router>
-    </AuthProvider>
+      <Wrapper>
+          <AuthProvider>
+              <Router></Router>
+          </AuthProvider>
+      </Wrapper>
   )
 }
 
